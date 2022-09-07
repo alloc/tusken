4f5e45abf288fc7feb49718c508c15d4e30ccfaa
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-09-07 17:17:25 -0400

    fix(put): allow rows to have missing columns
    If one row has a certain column, but that column is missing from another row, NULL will be used. In the case of a row conflict, the `coalesce` function will be used to preserve the column's current value, instead of overwriting it with NULL.

--- src/database/query/put.ts
+++ src/database/query/put.ts
@@ -2,7 +2,7 @@ import { ClientResult } from '../database'
 import { Query } from '../query'
 import { kPrimaryKey } from '../symbols'
 import { TableRef, toTableName } from '../table'
-import { TokenArray } from '../token'
+import { Token, TokenArray } from '../token'
 import { tokenize } from '../tokenize'
 
 type Props<T extends TableRef> = {
@@ -15,26 +15,29 @@ export class Put<T extends TableRef = any> extends Query<Props<T>, 'put'> {
   protected tokens(props: Props<T>, ctx: Query.Context) {
     let { table, data, pk } = props
 
-    const [columns, rows] = tokenizeRows(data, ctx)
+    const [columns, rows, nulls] = tokenizeRows(data, ctx)
     if (!rows.length) {
       throw Error('no rows to insert')
     }
 
     const pkColumn = table[kPrimaryKey]
-    if (pk) {
+    const mayConflict = pk !== undefined || columns.includes(pkColumn)
+
+    if (pk !== undefined) {
       columns.unshift(pkColumn)
       rows[0].tuple.unshift(tokenize(pk, ctx))
     }
 
+    const target = { id: toTableName(table) }
     const insertion: TokenArray = [
       'INSERT INTO',
-      { id: toTableName(table) },
+      mayConflict && nulls.size ? [target, 'this'] : target,
       { tuple: columns },
       'VALUES',
       { list: rows },
     ]
 
-    if (pk || columns.includes(pkColumn)) {
+    if (mayConflict) {
       insertion.push(
         'ON CONFLICT',
         { tuple: [table[kPrimaryKey]] },
@@ -42,7 +45,16 @@ export class Put<T extends TableRef = any> extends Query<Props<T>, 'put'> {
         {
           list: columns
             .filter(column => column !== pkColumn)
-            .map(k => [{ id: k }, '=', { id: ['excluded', k] }]),
+            .map(column => {
+              let value: Token = { id: ['excluded', column] }
+              if (nulls.has(column)) {
+                value = {
+                  callee: 'coalesce',
+                  args: [value, { id: ['this', column] }],
+                }
+              }
+              return [{ id: column }, '=', value]
+            }),
         }
       )
     }
@@ -62,26 +74,56 @@ export class Put<T extends TableRef = any> extends Query<Props<T>, 'put'> {
 export interface Put<T> extends PromiseLike<number> {}
 
 function tokenizeRows(data: Props<any>['data'], ctx: Query.Context) {
-  let columns!: string[]
   const rows: { tuple: TokenArray }[] = []
+
+  // This tracks which columns may have a NULL value
+  // for this specific INSERT command.
+  const nulls = new Set<string>()
+
+  let columns!: string[]
   for (const row of Array.isArray(data) ? data : [data]) {
     const values: TokenArray = []
     if (columns) {
       const newColumns = new Set(columns.concat(Object.keys(row)))
       for (const column of newColumns) {
-        values.push(tokenize(row[column], ctx))
+        if (column in row) {
+          tokenizeColumnValue(values, row[column], ctx)
+        } else {
+          // Use NULL to indicate this column should be left alone.
+          values.push('NULL')
+          nulls.add(column)
+        }
       }
-      if (newColumns.size > columns.length) {
+      let i = columns.length
+      if (i < newColumns.size) {
         columns = [...newColumns]
+        do {
+          // Each tuple's length must match the column count.
+          for (const row of rows) {
+            row.tuple.push('NULL')
+          }
+          nulls.add(columns[i])
+        } while (++i < newColumns.size)
       }
     } else {
       for (const column of (columns = Object.keys(row))) {
-        values.push(tokenize(row[column], ctx))
+        tokenizeColumnValue(values, row[column], ctx)
       }
     }
     rows.push({
       tuple: values,
     })
   }
-  return [columns, rows] as const
+
+  return [columns, rows, nulls] as const
+}
+
+function tokenizeColumnValue(
+  values: TokenArray,
+  value: any,
+  ctx: Query.Context
+) {
+  // For null and undefined values, use DEFAULT instead of NULL
+  // so that NULL can have another meaning (see further down).
+  values.push(value == null ? 'DEFAULT' : tokenize(value, ctx))
 }
