37f55db21a21c6ab008eee750521fd11021b449e
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-09-12 20:21:25 -0400

    chore: run tests before release

--- release.sh
+++ release.sh
@@ -1,4 +1,9 @@
+set -e
+pnpm -r build
+pnpm test:generate
+pnpm vitest
 pnpm dotenv -- multi-semantic-release --deps.release inherit $@
 pnpm install
 git add -u
 git commit -m "chore: update lockfile" --author "pnpm <https://pnpm.js.org>"
+git push
