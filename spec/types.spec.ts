aca165a82b20c55977f9b7fe2602f9b714d03f1c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-09-20 10:48:56 -0400

    test: print tsc output on non-zero exit code

--- spec/types.spec.ts
+++ spec/types.spec.ts
@@ -1,11 +1,13 @@
 import exec from '@cush/exec'
 
 test('TypeScript types', async () => {
-  const output = await exec('tsc --noEmit --project types/tsconfig.json', {
+  const typeChecker = exec('tsc --noEmit --project types/tsconfig.json', {
     cwd: __dirname,
     noThrow: true,
   })
-
-  expect(output).toMatchInlineSnapshot('""')
-  expect(output).toBe('')
+  const output = await typeChecker
+  if (typeChecker.exitCode !== 0) {
+    console.error(output)
+  }
+  expect(typeChecker.exitCode).toBe(0)
 })
