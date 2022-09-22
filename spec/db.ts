43861710fe2a08cba66784cd5c26d81516f099fb
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-09-22 17:14:39 -0400

    test: reset `e2e` database before each end-to-end test

--- spec/db.ts
+++ spec/db.ts
@@ -1,7 +1,27 @@
+import exec from '@cush/exec'
+import path from 'path'
 import { inspectQuery, Query } from 'tusken'
 import { expect } from 'vitest'
+import client from './generated/e2e'
 
-export { default, pg, t } from './generated/e2e'
+export { pg, t } from './generated/e2e'
+
+let db = client
+if (process.env.E2E) {
+  db = client.connect({ database: 'e2e' })
+
+  beforeEach(async () => {
+    await db.client.query('DROP SCHEMA public CASCADE; CREATE SCHEMA public')
+    await exec('psql -d e2e -f', [
+      path.resolve(__dirname, 'generated/test/schema.sql'),
+    ])
+    await exec('pg_restore -d e2e', [
+      path.resolve(__dirname, 'generated/test/data.dump'),
+    ])
+  })
+}
+
+export default db
 
 expect.addSnapshotSerializer({
   test: val => val instanceof Query,
