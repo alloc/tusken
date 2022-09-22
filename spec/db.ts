439033a63a03f2c5328134bf3b43667ec601a7e0
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-09-22 17:14:52 -0400

    test: add `enableQueryLogging` function

--- spec/db.ts
+++ spec/db.ts
@@ -7,8 +7,19 @@ import client from './generated/e2e'
 export { pg, t } from './generated/e2e'
 
 let db = client
+let shouldLogQueries = false
+
 if (process.env.E2E) {
   db = client.connect({ database: 'e2e' })
+
+  const { query } = db.client
+  db.client.query = (...args: any): any => {
+    if (shouldLogQueries && typeof args[0] == 'string') {
+      console.log('query: ', args[0])
+      console.log('params:', args[1])
+    }
+    return query.apply(db.client, args)
+  }
 
   beforeEach(async () => {
     await db.client.query('DROP SCHEMA public CASCADE; CREATE SCHEMA public')
@@ -22,6 +33,15 @@ if (process.env.E2E) {
 }
 
 export default db
+
+export const enableQueryLogging = () => {
+  console.log('Query logging enabled.')
+  shouldLogQueries = true
+}
+
+afterEach(() => {
+  shouldLogQueries = false
+})
 
 expect.addSnapshotSerializer({
   test: val => val instanceof Query,
