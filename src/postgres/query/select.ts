a313ae513838066a2cff9acae79e253d1689c68b
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-11-09 15:10:39 -0500

    refactor: move `stream` and async iterator support from SetBase to Select class
    …and ensure a proper result type.

--- src/postgres/query/select.ts
+++ src/postgres/query/select.ts
@@ -1,4 +1,7 @@
+import { renderQuery } from '../internal/query'
+import { Query } from '../query'
 import { Selectable, SelectResult, SelectResults } from '../selection'
+import { QueryStream, QueryStreamConfig } from '../stream'
 import { SetType } from '../type'
 import { SelectBase } from './base/select'
 import { Union } from './union'
@@ -13,6 +16,29 @@ export class Select<From extends Selectable[] = any> //
       props: { selects: [this, query] },
       query: new Union(this.db),
     })
+  }
+
+  stream(config?: QueryStreamConfig): QueryStream<SelectResult<From>> {
+    const ctx: Query.Context = {
+      query: this as any,
+      values: [],
+      resolvers: [],
+      mutators: [],
+    }
+
+    // TODO: apply mutators to stream
+    const query = renderQuery(ctx)
+    const client = this.db['getClient'](ctx)
+    if (client.stream) {
+      return client.stream(query, ctx.values, config)
+    }
+    throw Error('Streaming not supported by your Postgres client')
+  }
+
+  [Symbol.asyncIterator](): AsyncIterableIterator<SelectResult<From>> {
+    const stream = this.stream()
+    stream.resume()
+    return stream[Symbol.asyncIterator]()
   }
 }
 
