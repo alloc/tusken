3a2c2a5b567a3ca527087630a4d7de559df2942d
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-01-24 14:20:43 -0500

    feat: make check.ts exports public

--- src/tusken.ts
+++ src/tusken.ts
@@ -2,6 +2,7 @@ import './postgres/expression'
 
 export * from './config/defineConfig'
 export * from './definePlugin'
+export * from './postgres/check'
 export * from './postgres/column'
 export * from './postgres/connection'
 export * from './postgres/database'
